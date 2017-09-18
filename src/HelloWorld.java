2d814f25bf8212c66bcdde2929d70741c13b0375
HarpersGeekly <HarperRyanC@gmail.com>
2017-09-18 09:58:54 -0500

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * Created by RyanHarper on 9/18/17.
+ */
+public class HelloWorld {
+    //psvm = shortcut to write
+    //entry point, the beginning of the program
+    public static void main(String[] args) {
+        //public always needs to be there
+        //static always needs to be there
+        //void always needs to be there
+        //main, no change
+        //string[] no change
+        //args is the only thing that can change.
+        System.out.println("Hello World!");
+        System.out.print("Hello Redwood!");
+        //sout = shortcut to write
+    }
+}
+//compilation then execution.
