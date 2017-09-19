e742010e033f016f5a6867f764afd3f3acff4589
HarpersGeekly <HarperRyanC@gmail.com>
2017-09-19 13:56:21 -0500

    Exercises for Input Output

--- src/ConsoleIOExercises.java
+++ src/ConsoleIOExercises.java
@@ -5,16 +5,15 @@ import java.util.Scanner;
 
 public class ConsoleIOExercises {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
+        Scanner scan = new Scanner(System.in); // every scanner works with a delimiter -> ' ' by default
 //        Prompt a user to enter a integer and store that value in an int variable using the nextInt method.
         System.out.println("Enter an integer:");
         int userInt = scan.nextInt();
         System.out.println("you entered: " + userInt + ".");
 
 //        What happens if you input something that is not an integer?
-        // you get an error:
-        //Exception in thread "main" java.util.InputMismatchException
+        // you get an error in the stack trace, that says you inputted the wrong type, like a string, when it asked for an int:
+        //Exception in thread "main" java.util.InputMismatchException <--- that's the error
 //        at java.util.Scanner.throwFor(Scanner.java:864)
 //        at java.util.Scanner.next(Scanner.java:1485)
 //        at java.util.Scanner.nextInt(Scanner.java:2117)
@@ -28,9 +27,9 @@ public class ConsoleIOExercises {
 
 //        Prompt a user to enter 3 words and store each of them in a separate variable, then display them back, each on a newline.
         System.out.printf("Enter three words:");
-        String word1 = scan.next();
-        String word2 = scan.next();
-        String word3 = scan.next();
+        String word1 = scan.next(); // [enter] word1
+        String word2 = scan.next(); // [space] word2
+        String word3 = scan.next(); // [space] word3 [enter]
 //        What happens if you enter less than 3 words? It waits for the third word.
 //        What happens if you enter more than 3 words? The words after the third word gets taken into and used as the next "souted" line.
         System.out.println("You inputted: ");
@@ -44,55 +43,47 @@ public class ConsoleIOExercises {
         System.out.println("Enter a sentence");
         String userSentence = scan.next();
         System.out.println(userSentence);
+        System.out.println("using .next() only grabs the first word...");
 //        do you capture all of the words? No, not if you use next(), it only captures the first word in the sentence.
 
-        scan.nextLine();
+        scan.nextLine(); // use this after you use a .next() to cancel the "enter"
 
 //        Rewrite the above example using the .nextLine method.
         System.out.println("Enter a new sentence:");
         String userNewSentence = scan.nextLine();
         System.out.println("You typed: " + userNewSentence);
+        System.out.println("Using nextLine() grabs the whole sentence");
 
-//        Calculate the perimeter and area of Codeup's classrooms
+//        Calculate the perimeter and area of Codeup's classroom.
 //        Prompt the user to enter values of length and width of the classroom.
-        System.out.println("Enter the width of the room: ");
+//        Assume that the rooms are perfect rectangles.
+//        Assume that the user will enter valid numeric data for length and width.
+//        Accept decimal entries
+//        Display the perimeter and area of the classroom:
+        System.out.println("Let's calculate the perimeter and area of the classroom.");
+        System.out.println("Enter the width of the classroom: ");
         double width = scan.nextDouble();
-        System.out.println("Enter the length of the room: ");
+        System.out.println("Enter the length of the classroom: ");
         double length = scan.nextDouble();
-        System.out.println("The width is " + width + " and the length is " + length);
+        System.out.println("The width is " + width + " feet and the length is " + length + " feet.");
         double perimeter = ((length * 2) + (width * 2));
+        double areaOfRoom = (width * length);
         System.out.println("The perimeter is " + perimeter + " feet");
+        System.out.println("The area of the room is " + areaOfRoom + " square feet.");
+
+//        Calculate the volume of the room:
+        System.out.println("Now let's calculate the volume.");
+        System.out.println("Enter the height of the room");
+        double heightVolume = scan.nextDouble();
+        System.out.println("The width is " + width + " feet, the length is " + length + " feet, and the height is " + heightVolume + " feet.");
+        double volumeOfRoom = ((width * length) * heightVolume);
+        System.out.println("The volume of the room is " + volumeOfRoom + " cubic feet." );
 
         scan.nextLine();
 //        Use the .nextLine method to get user input and cast the resulting string to a numeric type.
         System.out.println("type in a number");
         int userGuess = Integer.parseInt(scan.nextLine());
         System.out.println(userGuess);
-
-//        Assume that the rooms are perfect rectangles.
-//        Assume that the user will enter valid numeric data for length and width.
-//        Accept decimal entries
-//        Display the area of tht classroom:
-        System.out.println("Enter the width of the room: ");
-        double widthArea = scan.nextDouble();
-        System.out.println("Enter the length of the room: ");
-        double lengthArea = scan.nextDouble();
-        System.out.println("The width is " + widthArea + " and the length is " + lengthArea);
-        double areaOfRoom = (widthArea * lengthArea);
-        System.out.println("The area of the room is " + areaOfRoom + " square feet");
-
-//        Calculate the volume of the room:
-
-        System.out.println("Enter the width of the room: ");
-        double widthVolume = scan.nextDouble();
-        System.out.println("Enter the length of the room: ");
-        double lengthVolume = scan.nextDouble();
-        System.out.println("Enter the height of the room");
-        double heightVolume = scan.nextDouble();
-        System.out.println("The width is " + widthVolume + " the length is " + lengthVolume + " and the height is " + heightVolume + ".");
-        double volumeOfRoom = ((widthVolume * lengthVolume) * heightVolume);
-        System.out.println("The volume of the room is " + volumeOfRoom + " cubic feet" );
-
     }
 }
 
