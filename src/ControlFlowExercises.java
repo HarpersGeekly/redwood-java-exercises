185bba34ca6f8ccd0880d75babf8f66549f8106d
HarpersGeekly <HarperRyanC@gmail.com>
2017-09-19 15:53:08 -0500

    Control Flow Exercise using loop basics: for, while, do while.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,4 @@
+
 /**
  * Created by RyanHarper on 9/19/17.
  */
@@ -74,49 +75,5 @@ public class ControlFlowExercises {
         for (long c = 2; c < 1000000; c *= c) {
             System.out.println(c);
         }
-
-
-//        Display a table of powers.
-//        Prompt the user to enter an integer.
-//        Display a table of squares and cubes from 1 to the value entered.
-//        Ask if the user wants to continue.
-//        Assume that the user will enter valid data.
-//        Only continue if the user agrees to.
-//        Example Output:
-//
-//        What number would you like to go up to? 5
-//
-//        Here is your table!
-//
-//        number | squared | cubed
-//        ------ | ------- | -----
-//        1      | 1       | 1
-//        2      | 4       | 8
-//        3      | 9       | 81
-//        4      | 16      | 64
-//        5      | 25      | 125
-
-
-
-
-
-//        Convert given number grades into letter grades.
-//        Prompt the user for a numerical grade from 0 to 100.
-//        Display the corresponding letter grade.
-//        Prompt the user to continue.
-//        Assume that the user will enter valid integers for the grades.
-//        The application should only continue if the user agrees to.
-//        Grade Ranges:
-//        A : 100 - 88
-//        B : 87 - 80
-//        C : 79 - 67
-//        D : 66 - 60
-//        F : 60 - 0
-
-
-//        Bonus
-//        Edit your grade ranges to include pluses and minuses (ex: 99-100 = A+).
-
-        
     }
 }
