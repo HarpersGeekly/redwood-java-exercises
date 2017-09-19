f4bdcbad03ec7453f99e4072a38880aa21786053
HarpersGeekly <HarperRyanC@gmail.com>
2017-09-19 14:31:32 -0500

    Control Flow Exercise using loop basics: for, while, do while.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -6,50 +6,83 @@ public class ControlFlowExercises {
 //    For all of the following exercises, create a new class named ControlFlowExercises with a main method. After each exercise, commit your changes, then replace your code in the main method with the next exercise.
 
 //        ==========================================   Loop Basics  ====================================================
-//        While
+//        While:
 //        Create an integer variable i with a value of 5.
 //        Create a while loop that runs so long as i is less than or equal to 15
 //        Each loop iteration, output the current value of i, then increment i by one.
         int i = 5;
 
-        while( i <= 15) {
+        while (i <= 15) {
             System.out.println("i = " + i++);
         }
 //        Your output should look like this:
 //        5 6 7 8 9 10 11 12 13 14 15
 
+//        ============================================  Do While =======================================================
 
-
-    }
-}
-
-
-//
-//        Do While
 //        Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+        System.out.println("=========================");
+        int x = 0;
+
+        do {
+            System.out.println(x);
+            x += 2;
+        } while (x <= 100);
+
 //        Alter your loop to count backwards by 5's from 100 to -10.
-//        Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
-//
-//        2
-//        4
-//        16
-//        256
-//        65536
-//        For
+        System.out.println("===================");
+
+        int y = 100;
+
+        do {
+            System.out.println(y);
+            y -= 5;
+        } while (y >= -10);
+
+//        Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000.
+        System.out.println("=========================");
+
+        long z = 2;
+        do {
+            System.out.println(z);
+            z *= z;
+        } while (z < 1000000);
+        // Output should equal:
+        //        2
+        //        4
+        //        16
+        //        256
+        //        65536
+
+//  ======================================================      For     ================================================
+
 //        refactor the previous two exercises to use a for loop instead
-//        Fizzbuzz
-//        One of the most common interview questions for entry-level programmers is the FizzBuzz test. Developed by Imran Ghory, the test is designed to test basic looping and conditional logic skills.
-//        Write a program that prints the numbers from 1 to 100.
-//        For multiples of three print “Fizz” instead of the number
-//        For the multiples of five print “Buzz”.
-//        For numbers which are multiples of both three and five print “FizzBuzz”.
+
+        System.out.println("=========================");
+
+        for (int a = 2; a <= 100; a++) {
+            System.out.println(a);
+        }
+        System.out.println("=========================");
+
+        for (int b = 5; b >= -10; b -= 5) {
+            System.out.println(b);
+        }
+
+        System.out.println("=========================");
+
+        for (long c = 2; c < 1000000; c *= c) {
+            System.out.println(c);
+        }
+
+
 //        Display a table of powers.
 //        Prompt the user to enter an integer.
 //        Display a table of squares and cubes from 1 to the value entered.
 //        Ask if the user wants to continue.
 //        Assume that the user will enter valid data.
 //        Only continue if the user agrees to.
-//        Exampmle Output
+//        Example Output:
 //
 //        What number would you like to go up to? 5
 //
@@ -62,6 +95,11 @@ public class ControlFlowExercises {
 //        3      | 9       | 81
 //        4      | 16      | 64
 //        5      | 25      | 125
+
+
+
+
+
 //        Convert given number grades into letter grades.
 //        Prompt the user for a numerical grade from 0 to 100.
 //        Display the corresponding letter grade.
@@ -74,5 +112,11 @@ public class ControlFlowExercises {
 //        C : 79 - 67
 //        D : 66 - 60
 //        F : 60 - 0
+
+
 //        Bonus
 //        Edit your grade ranges to include pluses and minuses (ex: 99-100 = A+).
+
+        
+    }
+}
