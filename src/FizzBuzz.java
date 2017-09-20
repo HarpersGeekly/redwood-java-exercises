06e25487fb611c5c58cdee1191479a43980c9309
HarpersGeekly <HarperRyanC@gmail.com>
2017-09-20 10:04:57 -0500

    Refactored with else if's

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -17,18 +17,14 @@ public class FizzBuzz {
         for (int x = 1; x <= 100; x++) {
             if (x % 3 == 0 && x % 5 == 0) {
                 System.out.println("FizzBuzz");
-            } else {
-                if (x % 3 == 0) {
+            } else if (x % 3 == 0) {
                     System.out.println("Fizz");
-                }
-                if (x % 5 == 0) {
+                } else if (x % 5 == 0) {
                     System.out.println("Buzz");
-                }
-                else {
+                } else {
                     System.out.println(x);
                 }
             }
         }
     }
-}
 
