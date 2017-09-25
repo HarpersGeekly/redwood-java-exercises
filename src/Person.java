4cb83c4ac847f2b6f2ff25b54a1cf26b3f84dd64
HarpersGeekly <HarperRyanC@gmail.com>
2017-09-25 09:27:31 -0500

    Add example of class and add client class to it

---
+++ src/Person.java
@@ -0,0 +1,20 @@
+/**
+ * Created by RyanHarper on 9/25/17.
+ */
+    // public = anyone has access to this class/attribute/function
+    // private = no-one can see this class/attribute/function. Kinda like in the in the real world.
+    // should the user be able to change it? What can the user use out of this object?
+    // We start thinking about a client and a server. The client is an object that calls a method on another object.
+// and the server is an object that does an operation on behalf of another object. We've been doing this already:
+
+    // objects talk to each other and collaborate! like System.out.println. or PrintStream out = System.out.
+// Scanner and System are the server (service to help me do something)... and I'm the client
+    //
+
+
+    // how do you want the client to use this class?
+public class Person {
+    public String firstName;
+    public String lastName;
+}
+
---
+++ src/PersonApplication.java
@@ -0,0 +1,17 @@
+/**
+ * Created by RyanHarper on 9/25/17.
+ */
+public class PersonApplication {
+    public static void main(String[] args) {
+
+        Person person = new Person();
+        person.firstName = "Ryan";
+        person.lastName = "Harper";
+        System.out.println("Hello, " + person.firstName + " " + person.lastName);
+        // API = Application Programming Interface
+        // REST API - Remote Program
+        // Object API. Every time you create an object you're creating an API, defining how your users will use an object.
+            // Any public attribute/method.
+        //
+    }
+}
