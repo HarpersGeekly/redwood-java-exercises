c19bf4e5dd4266769369f90f7b244b23ef7332fb
HarpersGeekly <HarperRyanC@gmail.com>
2017-10-06 14:27:30 -0500

    renamed from Collections so no conflict with real Collections util in java

--- src/CollectionsLecture.java
+++ src/CollectionsLecture.java
@@ -1,12 +1,9 @@
 /**
  * Created by RyanHarper on 10/2/17.
  */
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Set;
+import java.util.*;
 
-public class Collections {
+public class CollectionsLecture {
     public static void main(String[] args) {
 
         // Normal Array
@@ -17,7 +14,7 @@ public class Collections {
         HashMap<String, String> map = new HashMap<>();
 
 
-        if(names.isEmpty()){
+        if (names.isEmpty()) {
             System.out.println("Is empty");
         }
 
@@ -36,7 +33,7 @@ public class Collections {
         names.remove("Ryan");
 
         // Checks if the ArrayList has Ryan in it
-        if(names.contains("Ryan")){
+        if (names.contains("Ryan")) {
             System.out.println("It is in the list");
         }
 
@@ -49,14 +46,14 @@ public class Collections {
         // Tests
         System.out.println(map.get("Justin"));
         System.out.println(Arrays.binarySearch(lNames, "Harper"));
-        System.out.println("Index of zach " +names.indexOf("Zach"));
+        System.out.println("Index of zach " + names.indexOf("Zach"));
 
         // Prints the values for each array
-        for(String name : names){
+        for (String name : names) {
             System.out.println(name);
         }
 
-        for(String lName : lNames){
+        for (String lName : lNames) {
             System.out.println(lName);
         }
 
@@ -72,3 +69,5 @@ public class Collections {
 
     }
 }
+
+
