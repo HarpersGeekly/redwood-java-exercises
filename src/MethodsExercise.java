1adae49f44c0de19fed7e812cabfd60512528903
HarpersGeekly <HarperRyanC@gmail.com>
2018-07-26 12:04:02 -0500

    comments

--- src/MethodsExercise.java
+++ src/MethodsExercise.java
@@ -58,15 +58,15 @@ public class MethodsExercise {
 
 
     public static int multiply(int x, int y) {
-   /* 0  multiplied with anything gives 0 */
+   // 0  multiplied with anything gives 0
         if (y == 0) {
             return 0;
         }
-   /* Add x one by one */
+   // Add x one by one
         if (y > 0) {
             return (x + multiply(x, y - 1));
         }
-  /* the case where y is negative */
+  // the case where y is negative
         if (y < 0) {
             return -multiply(x, -y);
         }
